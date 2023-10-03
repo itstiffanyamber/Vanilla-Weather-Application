d00f6fb5e9aeffc6ef1c43a0719b6fc58c6f4e15
Tiffany-Amber <tiffanyamberjacobs@gmail.com>
2023-10-03 14:07:31 +0200

    Comments included

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-let celsiusTemperature = null;
-let current = new Date();
-let form = document.querySelector("#search-form");
-let celsiusLink = document.querySelector("#celsius-link");
+// Initialize variables
+let celsiusTemperature = null; // Variable to store temperature in Celsius
+let current = new Date(); // Current date
+let form = document.querySelector("#search-form"); // Search form element
+let celsiusLink = document.querySelector("#celsius-link"); // Celsius temperature link
 
+// Function to format the date and time
 function formatDate(now) {
   let hours = now.getHours();
   if (hours < 10) {
@@ -46,6 +48,7 @@ function formatDate(now) {
   return `${day}, ${month} ${date} | ${hours}:${minutes}`;
 }
 
+// Function to format the day of the week
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
@@ -54,6 +57,7 @@ function formatDay(timestamp) {
   return days[day];
 }
 
+// Function to display current weather data
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -83,6 +87,7 @@ function displayTemperature(response) {
   displayWeeklyForecast(response.data.coord); // Display weekly forecast
 }
 
+// Function to initiate a weather search
 function search(city) {
   let apiKey = "b46613281993492485b30d1857eea99b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -90,12 +95,14 @@ function search(city) {
   axios.get(apiUrl).then(displayTemperature);
 }
 
+// Function to handle form submission
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
 
+// Function to display temperature in Celsius
 function displayCelsiusTemperature(event) {
   event.preventDefault();
   celsiusLink.classList.add("active");
@@ -103,6 +110,7 @@ function displayCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
+// Function to display daily weather forecast
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
@@ -138,13 +146,7 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 
-function getForecast(coordinates) {
-  let apiKey = "b46613281993492485b30d1857eea99b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayForecast);
-}
-
+// Function to display weekly weather forecast
 function displayWeeklyForecast(coordinates) {
   let apiKey = "b46613281993492485b30d1857eea99b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
@@ -154,7 +156,16 @@ function displayWeeklyForecast(coordinates) {
   });
 }
 
-form.addEventListener("submit", handleSubmit);
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
+// Function to get and display weather forecast
+function getForecast(coordinates) {
+  let apiKey = "b46613281993492485b30d1857eea99b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
+// Event listeners
+form.addEventListener("submit", handleSubmit); // Listen for form submission
+celsiusLink.addEventListener("click", displayCelsiusTemperature); // Listen for Celsius temperature link click
+
+// Initial weather search for Cape Town
 search("Cape Town");
