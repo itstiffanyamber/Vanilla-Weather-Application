e9a37a3f247c365d8ef14739fd4b4d952b4638ce
Tiffany-Amber <tiffanyamberjacobs@gmail.com>
2023-10-04 19:38:35 +0200

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Vanilla-Weather-Application
- 
+
+This is a simple weather forecast web application that allows users to check the current weather and a 5-day forecast for a given city.
+
+## Features
+
+- Check current weather conditions including temperature, humidity, and wind speed.
+- Toggle between Celsius and Fahrenheit temperature units.
+- Search for weather data by entering a city name.
+- Get weather data for the user's current location using geolocation.
+- View a 5-day forecast including date, temperature, and weather description.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Axios (for making API requests)
+- Bootstrap (for styling)
+- Font Awesome (for icons)
+
+## Usage
+
+1. Enter the name of a city in the search input field.
+2. Click the "Search" button to retrieve weather data for that city.
+3. The weather information will be displayed, including temperature, humidity, and wind speed.
+4. You can toggle between Celsius and Fahrenheit temperature units by clicking the corresponding link.
+5. Click the location icon to get weather data for your current location.
+6. View a 5-day forecast below the current weather information.
+
+## Installation
+
+1. Clone this repository to your local machine:
--- src/app.js
+++ src/app.js
@@ -1,32 +1,6 @@
-// Initialize variables
-let celsiusTemperature = null; // Variable to store temperature in Celsius
-let current = new Date(); // Current date
-let form = document.querySelector("#search-form"); // Search form element
-let celsiusLink = document.querySelector("#celsius-link"); // Celsius temperature link
-let locationIcon = document.querySelector("#location-icon"); // Location icon
-
-// Event listeners
-form.addEventListener("submit", handleSubmit); // Listen for form submission
-celsiusLink.addEventListener("click", displayCelsiusTemperature); // Listen for Celsius temperature link click
-locationIcon.addEventListener("click", getCurrentLocationWeather); // Listen for location icon click
-
-// Initial weather search for Cape Town
-search("Cape Town");
-
-// Function to format the date and time
+// Function to format the date
 function formatDate(now) {
-  let hours = now.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-
-  let minutes = now.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
   let date = now.getDate();
-
   let days = [
     "Sunday",
     "Monday",
@@ -37,7 +11,6 @@ function formatDate(now) {
     "Saturday",
   ];
   let day = days[now.getDay()];
-
   let months = [
     "January",
     "February",
@@ -53,28 +26,13 @@ function formatDate(now) {
     "December",
   ];
   let month = months[now.getMonth()];
-
-  return `${day}, ${month} ${date} | ${hours}:${minutes}`;
+  return `${day}, ${month} ${date}`;
 }
 
-// Function to format the day of the week
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// Get the current date
+let current = new Date();
 
-  return days[day];
-}
-
-// Function to format hour
-function formatHour(timestamp) {
-  let date = new Date(timestamp);
-  let hour = date.getHours();
-  let minutes = date.getMinutes();
-  return `${hour}:${minutes < 10 ? "0" : ""}${minutes}`;
-}
-
-// Function to display current weather data
+// Function to display weather data
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -88,6 +46,7 @@ function displayTemperature(response) {
 
   celsiusTemperature = response.data.main.temp;
 
+  // Update HTML elements with weather data
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
   cityElement.innerHTML = response.data.name;
   weatherDescriptionElement.innerHTML = response.data.weather[0].description;
@@ -99,24 +58,12 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-
-  getForecast(response.data.coord);
-  displayWeeklyForecast(response.data.coord); // Display weekly forecast
 }
 
-// Function to display temperature in Celsius
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-// Function to initiate a weather search
+// Function to search for weather by city
 function search(city) {
-  let apiKey = "b46613281993492485b30d1857eea99b";
+  let apiKey = "f91094f9e909c81cb1790c946625748c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
   axios.get(apiUrl).then(displayTemperature);
 }
 
@@ -127,118 +74,43 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-// Function to display daily weather forecast
-function displayForecast(response) {
-  let forecast = response.data.daily;
-  let forecastElement = document.querySelector("#forecast");
-
-  let forecastHTML = `<div class="row">`;
-
-  forecast.forEach(function (forecastDay, index) {
-    if (index < 7) {
-      forecastHTML += `
-        <div class="col">
-          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-          <img
-            src="https://openweathermap.org/img/wn/${
-              forecastDay.weather[0].icon
-            }@2x.png"
-            alt=""
-            width="42"
-          />
-          <div class="weather-forecast-temperature">
-            <span class="weather-forecast-temperature-max"> ${Math.round(
-              forecastDay.temp.max
-            )}°</span>
-            <span class="weather-forecast-temperature-min"> ${Math.round(
-              forecastDay.temp.min
-            )}°</span>
-          </div>
-        </div>
-      `;
-    }
-  });
-
-  forecastHTML += `</div>`;
-  forecastElement.innerHTML = forecastHTML;
+// Function to display temperature in Celsius
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-// Function to display weekly weather forecast
-function displayWeeklyForecast(coordinates) {
-  let apiKey = "b46613281993492485b30d1857eea99b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+// Initialize Celsius temperature variable
+let celsiusTemperature = null;
 
-  axios.get(apiUrl).then(function (response) {
-    displayForecast(response);
-  });
-}
+// Get the search form and add a submit event listener
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
-// Function to get and display weather forecast
-function getForecast(coordinates) {
-  let apiKey = "b46613281993492485b30d1857eea99b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
-}
+// Get the Celsius link and add a click event listener
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-// Function to get and display hourly forecast
-function getHourlyForecast(coordinates) {
-  let apiKey = "b46613281993492485b30d1857eea99b";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+// Default search for "Port Elizabeth" weather
+search("Port Elizabeth");
 
-  axios.get(apiUrl).then(displayHourlyForecast);
-}
-
-// Function to display hourly forecast
-function displayHourlyForecast(response) {
-  let hourlyForecast = response.data.hourly;
-  let hourlyForecastContainer = document.querySelector("#hourly-forecast");
-
-  // Clear previous forecast data
-  hourlyForecastContainer.innerHTML = "";
-
-  // Iterate through the hourly forecast data and display it
-  for (let i = 0; i < 4 * 24; i += 8) {
-    let forecastHour = hourlyForecast[i];
-    let forecastTimestamp = forecastHour.dt * 1000;
-    let forecastIcon = forecastHour.weather[0].icon;
-
-    let day = formatDay(forecastTimestamp);
-    let hour = formatHour(forecastTimestamp);
-
-    // Create a card for each day's forecast
-    let forecastCard = document.createElement("div");
-    forecastCard.classList.add("col");
-    forecastCard.innerHTML = `
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">${day}</h5>
-          <p class="card-text">${hour}</p>
-          <img
-            src="https://openweathermap.org/img/wn/${forecastIcon}.png"
-            alt=""
-            width="50"
-          />
-        </div>
-      </div>
-    `;
-
-    hourlyForecastContainer.appendChild(forecastCard);
-  }
-}
-
-// Function to get current location weather
+// Function to get weather for the current location
 function getCurrentLocationWeather() {
   if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(function (position) {
       let latitude = position.coords.latitude;
       let longitude = position.coords.longitude;
-
-      let apiKey = "b46613281993492485b30d1857eea99b";
+      let apiKey = "f91094f9e909c81cb1790c946625748c";
       let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
       axios.get(apiUrl).then(displayTemperature);
     });
   } else {
     alert("Geolocation is not supported by your browser.");
   }
 }
+
+// Add click event listener to the location icon
+let locationIcon = document.querySelector("#location-icon");
+locationIcon.addEventListener("click", getCurrentLocationWeather);
